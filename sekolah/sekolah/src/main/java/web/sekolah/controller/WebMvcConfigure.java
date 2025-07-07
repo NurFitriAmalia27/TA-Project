6290c43446479f2043ec2329e118d7d44d49e4c1
Anisa Amelia <anisaamelia739@gmail.com>
2025-07-07 18:38:50 +0700

    update footer

--- sekolah/sekolah/src/main/java/web/sekolah/controller/WebMvcConfigure.java
+++ sekolah/sekolah/src/main/java/web/sekolah/controller/WebMvcConfigure.java
@@ -10,39 +10,43 @@ public class WebMvcConfigure implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         // Konfigurasi untuk folder guru
         registry.addResourceHandler("/img/guru/**")
-                .addResourceLocations("file:C:/Tugas Akhir/sekolah/sekolah/src/main/resources/static/img/guru/")
+                .addResourceLocations("file:C:/SpringBoot-TA/sekolah/sekolah/src/main/resources/static/img/guru/")
                 .setCachePeriod(0); // Cache di-disable agar gambar langsung terupdate
 
         // Konfigurasi untuk folder ekstrakurikuler
         registry.addResourceHandler("/img/ekstrakurikuler/**")
-                .addResourceLocations("file:C:/Tugas Akhir/sekolah/sekolah/src/main/resources/static/img/ekstrakurikuler/")
+                .addResourceLocations("file:C:/SpringBoot-TA/sekolah/sekolah/src/main/resources/static/img/ekstrakurikuler/")
                 .setCachePeriod(0); // Cache di-disable agar gambar langsung terupdate
 
         // Konfigurasi untuk folder berita
         registry.addResourceHandler("/img/berita/**")
+
                 .addResourceLocations("file:C:/Tugas akhir/sekolah/sekolah/src/main/resources/static/img/berita/")
+
+                .addResourceLocations("file:C:/SpringBoot-TA/sekolah/sekolah/src/main/resources/static/img/berita/")
+
                 .setCachePeriod(0); // Cache di-disable agar gambar langsung terupdate
 
         // Konfigurasi untuk folder berita
 
         registry.addResourceHandler("/img/prestasi-guru/**")
-                .addResourceLocations("file:C:/Tugas Akhir/sekolah/sekolah/src/main/resources/static/img/prestasi-guru/")
+                .addResourceLocations("file:C:/SpringBoot-TA/sekolah/sekolah/src/main/resources/static/img/prestasi-guru/")
                 .setCachePeriod(0); // Cache di-disable agar gambar langsung terupdate
 
         registry.addResourceHandler("/img/buku/**")
-                .addResourceLocations("file:C:/Tugas Akhir/sekolah/sekolah/src/main/resources/static/img/buku/")
+                .addResourceLocations("file:C:/SpringBoot-TA/sekolah/sekolah/src/main/resources/static/img/buku/")
                 .setCachePeriod(0);
 
         registry.addResourceHandler("/img/prestasi-siswa/**")
-                .addResourceLocations("file:C:/Tugas Akhir/sekolah/sekolah/src/main/resources/static/img/prestasi-siswa/")
+                .addResourceLocations("file:C:/SpringBoot-TA/sekolah/sekolah/src/main/resources/static/img/prestasi-siswa/")
                 .setCachePeriod(0);
 
         registry.addResourceHandler("/img/prestasi-sekolah/**")
-                .addResourceLocations("file:C:/Tugas Akhir/sekolah/sekolah/src/main/resources/static/img/prestasi-sekolah/")
+                .addResourceLocations("file:C:/SpringBoot-TA/sekolah/sekolah/src/main/resources/static/img/prestasi-sekolah/")
                 .setCachePeriod(0);
 
         registry.addResourceHandler("/img/laporanbuku/**")
-                .addResourceLocations("file:C:/Tugas Akhir/sekolah/sekolah/src/main/resources/static/img/laporanbuku/")
+                .addResourceLocations("file:C:/SpringBoot-TA/sekolah/sekolah/src/main/resources/static/img/laporanbuku/")
                 .setCachePeriod(0);
     }
 }
